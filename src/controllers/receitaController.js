c62a61f2ee7c6abf7bc1e5f3cd47023609b91190
cristianohugo <32584307+cristianohugo@users.noreply.github.com>
2021-07-13 00:09:35 -0300

    Implementados select, update e delete

--- src/controllers/receitaController.js
+++ src/controllers/receitaController.js
@@ -31,3 +31,90 @@ exports.Insert = (req, res, next) => {
         //catch = registra o que queremos que aconteca quando a Promise falhar
         .catch(error => next(error));
 };
+
+exports.SearchAll = (req, res, next) => {
+    Receita.findAll()
+        .then(receita => {
+            if (receita) {
+                res.status(status.OK).send(receita);
+            }
+        })
+        .catch(error => next(error));
+}
+
+exports.SearchOne = (req, res, next) => {
+    const id = req.params.id;
+
+    Receita.findByPk(id)
+        .then(receita => {
+            if (receita) {
+                res.status(status.OK).send(receita);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+
+//atualizar os dados
+exports.Update = (req, res, next) => {
+    //na requisicao de atualizar
+    //quando atualizamos enviamos o id, que vai ser pego da url
+    const id = req.params.id;
+    const descricao = req.body.descricao;
+    const valor = req.body.valor;
+    const data = req.body.data;
+    const recebido = req.body.recebido;
+
+    Receita.findByPk(id)
+        //primeiro precisamos verificar se o dado existe
+        //then = registra o que queremos que aconteca quando a Promise for resolvida
+        .then(receita => {
+            if (receita) {
+                //se existir, vai atualizar
+                //passa um objeto com as infos
+                receita.update({
+                    descricao: descricao,
+                    valor: valor,
+                    data: data,
+                    recebido: recebido
+                },
+                    //recebe um parametro id na clausula where
+                    {
+                        where: { id: id }
+                    })
+                    .then(() => {
+                        //status 200 é o padrao
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            } else {
+                //caso nao existir, retorna erro
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        //catch = registra o que queremos que aconteca quando a Promise falhar
+        .catch(error => next(error));
+};
+
+
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+
+    Receita.findByPk(id)
+        .then(receita => {
+            if (receita) {
+                receita.destroy({
+                    where: { id: id }
+                })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            }
+            else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,9 @@ const ReceitaController = require ('../controllers/receitaController.js');
 const router = express.Router();
 
 router.post('/receita', ReceitaController.Insert);
+router.get('/receita', ReceitaController.SearchAll);
+router.get('/receita/:id', ReceitaController.SearchOne);
+router.put('/receita/:id', ReceitaController.Update);
+router.delete('/receita/:id', ReceitaController.Delete);
 
 module.exports = router;
